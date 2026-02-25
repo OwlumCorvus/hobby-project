19924a200adc0d0b2695bc4c404653680eebe7b6
Owlum <zachrrodgers@gmail.com>
2026-02-25 17:57:31 -0500

    Fixed navigation arrival and added interaction progressbar

--- player.gd
+++ player.gd
@@ -6,8 +6,9 @@ var target_interactable = null
 
 @export var speed: float = 200.0
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var interaction_bar = $InteractionBar
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	match current_state:
 		State.MOVING:
 			_process_movement()
@@ -15,7 +16,9 @@ func _physics_process(delta: float) -> void:
 			_process_interaction()
 
 func _process_movement():
+	print("distance: " + str(nav_agent.distance_to_target()))
 	if nav_agent.is_navigation_finished():
+		print("Nav Complete!")
 		if target_interactable:
 			current_state = State.INTERACTING
 		else:
@@ -28,26 +31,36 @@ func _process_movement():
 	velocity = new_velocity
 	move_and_slide()
 	
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			move_to_pos(get_global_mouse_position())
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event is InputEventMouseButton:
+		#if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			#move_to_pos(get_global_mouse_position())
 
 func move_to_pos(target: Vector2):
+	print("Moving to: " + str(target))
 	target_interactable = null
 	nav_agent.target_position = target
 	current_state = State.MOVING
 
 func _process_interaction():
-	if target_interactable:
-		await get_tree().create_timer(2.0).timeout
-		target_interactable = null
-		current_state = State.IDLE
+	
+	velocity = Vector2.ZERO
+	print("visible")
+	interaction_bar.visible = true
+	interaction_bar.value = 0
+		
+	var tween = create_tween()
+	tween.tween_property(interaction_bar, "value", 100, 2.0)
+		
+	await get_tree().create_timer(2.0).timeout
+	
+	print("done")
+	interaction_bar.visible = false
+	target_interactable = null
+	current_state = State.IDLE
 
 func interact_with_object(object: Interactable):
+	print("Moving to: " + object.object_name)
 	target_interactable = object
 	nav_agent.target_position = object.global_position
 	current_state = State.MOVING
-
-func _on_tree_interacted(destination_pos: Variant) -> void:
-	pass # Replace with function body.
--- world.gd
+++ world.gd
@@ -2,14 +2,29 @@ extends Node2D
 
 @onready var player = $Player
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Tree.interacted.connect(_on_tree_clicked)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_tree_clicked(tree_object):
 	player.interact_with_object(tree_object)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			var clicked_object = _get_clicked_object()
+			if clicked_object is Interactable:
+				player.interact_with_object(clicked_object)
+			else:
+				player.move_to_pos(get_global_mouse_position())
+				
+func _get_clicked_object():
+	var space_state = get_world_2d().direct_space_state
+	var params = PhysicsPointQueryParameters2D.new()
+	params.position = get_global_mouse_position()
+	params.collide_with_areas = true
+	params.collide_with_bodies = true
+	
+	var results = space_state.intersect_point(params)
+	if results.size() > 0:
+		return results[0].collider
+	return null
