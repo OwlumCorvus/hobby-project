51e4a7c77348aea4e5ef15805f1a2c73d3831175
Owlum <zachrrodgers@gmail.com>
2026-02-27 14:42:41 -0500

    fix: updated object interaction script

--- interactable.gd
+++ interactable.gd
@@ -1,19 +1,44 @@
 extends StaticBody2D
 class_name Interactable
 
-@export var object_name: String = "Generic Object"
+@onready var object_progress_bar: TextureProgressBar = $ObjectProgressBar
+@onready var action_timer: Timer = $ActionTimer
+@onready var depleted_timer: Timer = $DepletedTimer
+@onready var object_label: Label = $ObjectLabel
 
-signal interacted(object: Interactable)
+var is_near: bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	self.input_event.connect(_on_input_event)
 	input_pickable = true
+	object_progress_bar.value = 0
+	
 
-func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			interacted.emit(self)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if not action_timer.is_stopped():
+		object_progress_bar.value = (1 - (action_timer.wait_time - action_timer.time_left) / action_timer.wait_time) * 100
+	else:
+		object_progress_bar.value = 0
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	get_viewport().set_input_as_handled()
+	if event is InputEventMouseButton and event.pressed:
+		if is_near and event.button_index == MOUSE_BUTTON_LEFT:
+			if action_timer.is_stopped():
+				object_progress_bar.visible = true
+				action_timer.start()
+			else:
+				action_timer.stop()
+				object_progress_bar.visible = false
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		is_near = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		is_near = false
+		action_timer.stop()
+		object_progress_bar.visible = false
