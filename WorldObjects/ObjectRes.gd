d3b095afb1f7d174fe0912ef575c035f9b2e84f6
Owlum <zachrrodgers@gmail.com>
2026-03-02 16:24:56 -0500

    feat: created resource script for objects

---
+++ WorldObjects/ObjectRes.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name ObjectResource
+
+@export var obj_name: String
+@export_multiline var obj_desc: String
+@export var obj_locations: Array[String] # Change string to Location Resource
+@export var obj_items: Array[String] # Change string to item Resource
+@export var obj_capacity: int
+@export var obj_wait_time_depleted: float
+@export var obj_wait_time_action: float
+@export var obj_sprites: Array[Texture2D]
+@export var obj_sounds: String # Change string to Array[audio type]
+@export var obj_animations: String # Change string to sprite sheet
