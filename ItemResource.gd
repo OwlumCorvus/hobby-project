fbb35a32a2fbf659db484f09da386348d3f7ea65
Owlum <zachrrodgers@gmail.com>
2026-03-03 09:33:05 -0500

    feat: created item resource script

---
+++ ItemResource.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name ItemData
+
+@export var itm_name: String
+@export_multiline var itm_desc: String
+@export var itm_sprite: Texture2D
+@export var itm_animation: String
+@export var itm_type: Array[String]
+@export var itm_stat_mod: int
--- WorldObjects/ObjectRes.gd
+++ WorldObjects/ObjectRes.gd
@@ -4,8 +4,10 @@ class_name ObjectResource
 @export var obj_name: String
 @export_multiline var obj_desc: String
 @export var obj_locations: Array[String] # Change string to Location Resource
-@export var obj_items: Array[String] # Change string to item Resource
+@export var obj_items: Array[ItemData]
 @export var obj_capacity: int
+@export var obj_exp_given: float
+ # @export var obj_lvl_required: int
 @export var obj_wait_time_depleted: float
 @export var obj_wait_time_action: float
 @export var obj_sprites: Array[Texture2D]
